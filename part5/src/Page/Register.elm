7270fbf8b07206611786acefe4a854728c25eae6
Daniyar <ddr199305@gmail.com>
2018-10-16 17:47:04 +0200

    Solve task #5

--- part5/src/Page/Register.elm
+++ part5/src/Page/Register.elm
@@ -87,11 +87,9 @@ viewForm form =
                 [ class "form-control form-control-lg"
                 , placeholder "Username"
 
-                {- 👉 TODO: when the user inputs a username, update it in the Model.
-
-                   💡 HINT: Look at how the Email input below does this. 👇
-                -}
+                {- 👉 TODO: when the user inputs a username, update it in the Model. -}
                 , value form.username
+                , onInput EnteredUsername
                 ]
                 []
             ]
@@ -141,6 +139,7 @@ type Msg
     = SubmittedForm
     | EnteredEmail String
     | EnteredPassword String
+    | EnteredUsername String
     | CompletedRegister (Result Http.Error Viewer)
     | GotSession Session
 
@@ -153,6 +152,9 @@ update msg model =
 
         EnteredPassword password ->
             updateForm (\form -> { form | password = password }) model
+
+        EnteredUsername username ->
+            updateForm (\form -> { form | username = username }) model
 
         SubmittedForm ->
             case validate model.form of
